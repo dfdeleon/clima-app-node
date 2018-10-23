2eac4ec6ae40596c1be8f74a3ef7ff1be3b73940
dfdeleon <floresdl.david@gmail.com>
2018-10-22 21:35:28 -0500

    Segundo commit

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // const axios = require('axios');
-
+const colors = require('colors/safe');
 const lugar = require('./lugar/lugar');
 const clima = require('./clima/clima');
 
@@ -18,9 +18,9 @@ let getInfo = async(direccion) => {
         let coord = await lugar.getLugarLatLng(direccion);
         let temp = await clima.getClima(coord.latitud, coord.longitud);
 
-        return (`El clima en ${coord.direccion} es de ${temp}°`)
+        return (`El clima en ${colors.cyan(coord.direccion)} es de ${colors.cyan(temp)}°`)
     } catch (e) {
-        return `No se pudo determinar el clima en ${direccion}`;
+        return `No se pudo determinar el clima en ${colors.red(direccion)}`;
     }
 
 }
